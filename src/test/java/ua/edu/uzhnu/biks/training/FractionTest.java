7e8881ee13abaf85e4c0dc1d24c26550804002c3
Actine <actinate@gmail.com>
2017-03-02 08:55:28 +0200

    Додав у проект JUnit, написав пробний тест

---
+++ src/test/java/ua/edu/uzhnu/biks/training/FractionTest.java
@@ -0,0 +1,16 @@
+package ua.edu.uzhnu.biks.training;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Actine on 02.03.2017.
+ */
+public class FractionTest {
+
+    @Test
+    public void test_SanityTest_JUnitWorks() {
+        Assert.assertEquals(2, 1 + 1);
+    }
+
+}
